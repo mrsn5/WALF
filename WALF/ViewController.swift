d3564552faf1304eeb24de176c0ab376e918ae94
San Nguyen <mr.sn5.kma@gmail.com>
2020-02-17 15:14:05 +0200

    Initial Commit

---
+++ WALF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WALF
+//
+//  Created by San Byn Nguyen on 17.02.2020.
+//  Copyright © 2020 San Byn Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WALFTests/WALFTests.swift
@@ -0,0 +1,34 @@
+//
+//  WALFTests.swift
+//  WALFTests
+//
+//  Created by San Byn Nguyen on 17.02.2020.
+//  Copyright © 2020 San Byn Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import WALF
+
+class WALFTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
